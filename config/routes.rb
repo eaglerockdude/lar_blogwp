a7d60ebc404eea0a780b0b12bae845edbf23a4d5
eaglerockdude <kendmcfadden@gmail.com>
2015-08-19 13:30:38 -0700

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+
+  get 'about', to: 'pages#about'
+
   root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
